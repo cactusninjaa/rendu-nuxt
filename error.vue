808685682d5cac8f75bda0a7a3e325b533b84318
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-12-13 11:42:28 +0100

    EDIT error

--- error.vue
+++ error.vue
@@ -1,18 +1,18 @@
 <template>
   <NuxtLayout name="header">
-    <div class="prose">
+    <div class="prose max-w-xl mx-auto">
       <template v-if="error.statusCode === 404">
         <template v-if="error.url && error.url.startsWith('/quizz-')">
-          <h1>404!</h1>
+          <h1 class="text-3xl text-red-500">404!</h1>
           <p>Désolé, le quiz que vous cherchez n'existe pas.</p>
         </template>
         <template v-else>
-          <h1>404!</h1>
-          <p>Sorry, that page doesn't exist.</p>
+          <h1 class="text-3xl text-red-500">404!</h1>
+          <p>Sorry, that page doesn't exist.</p>gi
         </template>
       </template>
       <template v-else>
-        <h1>Dang</h1>
+        <h1 class="text-3xl text-red-500">Dang</h1>
         <p>
           <strong>{{ error.message }}</strong>
         </p>
