432b405b5f23794ef28de4799be0c26918048b4f
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-12-13 14:20:48 +0100

    ADD quizzCard

---
+++ components/quizzCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const quizzs = inject('quizzs')
+</script>
+
+<template>
+    <div>
+        <div v-for="quizz in quizzs.quizzs" :key="quizz._id">
+            <div v-if="quizz.tags.includes(quizzs.selectedFilter.value) || quizzs.selectedFilter.value === ''"
+            class="flex flex-col bg-gray-200 p-4 gap-2 rounded-md shadow my-6">
+                <h3 class="text-xl">{{ quizz.title }}</h3>
+                <p class="text-sm">Nombre de questions : {{ quizz.questions.length }}</p>
+                <NuxtLink 
+                    :to="`/${quizz._path.split('/').pop()}`"
+                    class="bg-blue-500 text-white p-2 rounded-md text-center"
+                >
+                    Start
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
--- components/quizzCards.vue
+++ components/quizzCards.vue
@@ -9,18 +9,6 @@
             <option :value="tag" v-for="tag in quizzs.uniqueTags.value"  :key="tag">{{ tag }}</option>
         </select>
         
-        <div v-for="quizz in quizzs.quizzs" :key="quizz._id">
-            <div v-if="quizz.tags.includes(quizzs.selectedFilter.value) || quizzs.selectedFilter.value === ''"
-                class="flex flex-col bg-gray-200 p-4 gap-2 rounded-md shadow my-6">
-                <h3 class="text-xl">{{ quizz.title }}</h3>
-                <p class="text-sm">Nombre de questions : {{ quizz.questions.length }}</p>
-                <NuxtLink 
-                    :to="`/${quizz._path.split('/').pop()}`"
-                    class="bg-blue-500 text-white p-2 rounded-md text-center"
-                >
-                    Start
-                </NuxtLink>
-            </div>
-        </div>
+        <QuizzCard />
     </div>
 </template>
